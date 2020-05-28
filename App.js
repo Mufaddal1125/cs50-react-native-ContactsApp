2c93ee58bf70961862419fcf3b6cf783399e313d
Mufaddal Patanwala <mufaddal1125@gmail.com>
2020-05-28 10:00:23 +0530

    update

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  FlatList,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Constants } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Row from "./row";
+import contacts, { compareNames } from "./contacts";
+
+export default class App extends React.Component {
+  state = {
+    showContacts: true,
+  };
+
+  toggleContacts = () => {
+    this.setState((prevState) => ({ showContacts: !prevState.showContacts }));
+  };
+
+  renderItem = (obj) => <Row {...obj.item} />;
+  
+  sort = () => {
+    this.setState((prevState) => {
+      contacts: [...prevState.contacts].sort(compareNames);
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <h1>Hello World!!!!</h1>
+        <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
+
+        {this.state.showContacts ? (
+          <FlatList data={this.state.contacts} renderItem={this.renderItem} />
+        ) : null}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // paddingTop: Constants.statusBarHeight,
   },
 });
